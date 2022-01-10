63b2d84cefb6bf421e9a47b7f17a869d46d493e2
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-10 18:35:07 +0530

    #3 New router for displaying sessions of individual subjects

--- src/routers/views.js
+++ src/routers/views.js
@@ -8,6 +8,7 @@ const Subjects = require('../models/subjects')
 // Functions Start, remove to another file later
 
 // End of functions and start of routers
+// GET Homepage
 router.get('/', async (req, res) => {
     try {
         allSubjects = await Subjects.allSubjectList()
@@ -15,6 +16,10 @@ router.get('/', async (req, res) => {
     } catch (error) {
         console.log(error)
     }
+})
+// GET Individual Subject Page
+router.get('/subjectSessions?:uid',async(req,res)=>{
+    console.log(req.query.uid)
 })
 
 module.exports = router
