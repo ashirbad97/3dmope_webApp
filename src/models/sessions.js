6f22d13b10bf478318b4cbd87b94c0b5bb5b90cc
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-10 18:50:49 +0530

    Schema for sessions and static function to find all sessions for the subject

---
+++ src/models/sessions.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const sessionSchema = new mongoose.Schema({
+    sessionId:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    timestamp:{
+        type: Date,
+        required: true
+    },
+    subjectId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref:'Subjects'
+    },
+    trialFilesDir:{
+        type: String,
+        trim: true
+    },
+    outputFilesDir:{
+        type: String,
+        trim: true
+    }
+})
+
+sessionSchema.statics.findSessionOfSubject = async(subjectId)=>{
+    try {
+        subjectSessions = Sessions.find(subjectId).count()
+    } catch (error) {
+        
+    }
+}
+
+const Sessions = mongoose.model('Sessions',sessionSchema)
+module.exports = Sessions
