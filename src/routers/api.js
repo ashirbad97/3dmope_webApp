ec269e0bf68b2320159ee544154e0530057d4de0
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-10 08:05:45 +0530

    Fixed errors

--- src/routers/api.js
+++ src/routers/api.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const apirouter = new express.Router()
+const path = require('path')
+const sqlite3 = require('sqlite3').verbose();
+// Functions are to be moved to another file later
+// Path to database file
+db_directory = path.join(__dirname, '../db/3dmoper.db')
+// Defining connection to the on-disk db
+let db = new sqlite3.Database(db_directory, (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log("Successfully connected to database")
+    }
+});
 
+// End of functions and start of routers
 apirouter.post('/api/createUser', async (req, res) => {
     try {
         console.log(req.body)
