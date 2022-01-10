5a958a70f0c3df4b2d6c8096f6b50ddb524af63a
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-10 18:17:14 +0530

    #2 Shows List of subjects

--- src/routers/views.js
+++ src/routers/views.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = new express.Router()
+const path = require('path')
 
+// Importing Models
+const Subjects = require('../models/subjects')
+
+// Functions Start, remove to another file later
+
+// End of functions and start of routers
 router.get('/', async (req, res) => {
     try {
-        res.render('adminHome')
+        allSubjects = await Subjects.allSubjectList()
+        res.render('adminHome',{allSubjects})
     } catch (error) {
         console.log(error)
     }
