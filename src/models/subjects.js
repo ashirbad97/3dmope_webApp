a6e0aa330f122a3eec890bfae1e0ad20f6c2fb59
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-11 12:24:49 +0530

    Static schema func to check if user exist

--- src/models/subjects.js
+++ src/models/subjects.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose')
 
 const subjectSchema = new mongoose.Schema({
-    subjectId:{
+    subjectId: {
         type: String,
         required: true,
         trim: true,
         unique: true
     },
-    addnInfo:{
-        type:String,
-        trim:true
+    addnInfo: {
+        type: String,
+        trim: true
     }
 })
 // Schema Statics
-subjectSchema.statics.allSubjectList = async()=>{
+subjectSchema.statics.allSubjectList = async () => {
     subjectList = await Subjects.find().select('subjectId')
     return subjectList
 }
 
-subjectSchema.statics.createNewUser = async(uid)=>{
+subjectSchema.statics.createNewUser = async (uid) => {
     try {
         newSubject = await Subjects({
-            subjectId : uid
+            subjectId: uid
         })
         await newSubject.save()
         return true
@@ -32,5 +32,20 @@ subjectSchema.statics.createNewUser = async(uid)=>{
 
 }
 
-const Subjects = mongoose.model('Subjects',subjectSchema)
+subjectSchema.statics.findUser = async (subjectId) => {
+    try {
+        ifUser = await Subjects.findOne({ subjectId })
+        if (ifUser) {
+            console.log("Found User ", subjectId)
+            return true
+        } else {
+            console.log("Subject Not Found ", subjectId)
+            return false
+        }
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+const Subjects = mongoose.model('Subjects', subjectSchema)
 module.exports = Subjects
