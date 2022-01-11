4da2392d7daee7499488c8049fd0b17f3fc8b99b
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-11 19:17:47 +0530

    Big ambiguity, due to some error the find function working with Postman API calls but not with Unity

--- src/models/subjects.js
+++ src/models/subjects.js
@@ -34,8 +34,10 @@ subjectSchema.statics.createNewUser = async (uid) => {
 
 subjectSchema.statics.findUser = async (uid) => {
     try {
-        ifUser = await Subjects.find({ subjectId: String(uid) })
-        console.log(ifUser)
+        // Big ambiguity, due to some error the find function working with Postman API calls but not with Unity
+        // so have to manually fetch all the users and check among them
+        ifUser = await Subjects.find()
+        console.log(ifUser.subjectId)
         if (ifUser) {
             console.log("Found User ", uid)
             return true
