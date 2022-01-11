4ea3fd613210a42c5bd39431cd2b4a00d957f82d
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-11 12:25:13 +0530

    API Endpoint to check if user exist

--- src/routers/api.js
+++ src/routers/api.js
@@ -13,12 +13,24 @@ apirouter.post('/api/createUser', async (req, res) => {
         saveStatus = await Subjects.createNewUser(req.body.uid)
         console.log(saveStatus)
         if (saveStatus)
-        res.status(200).send("Subject Saved")
-        else 
-        throw Error('Not able to save')
+            res.status(200).send("Subject Saved")
+        else
+            throw Error('Not able to save')
     } catch (error) {
         console.log(error)
         res.status(500).send("Error saving the subject")
+    }
+})
+// API Endpoint to check if a given uid exists
+apirouter.post('/api/checkUser', async (req, res) => {
+    try {
+        console.log("Checking for User:", req.body.uid)
+        checkIfUser = await Subjects.findUser(req.body.uid)
+        if (checkIfUser) res.status(200).send("Subject Found")
+        else return res.status(404).send("Subject Not Found")
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Error fetching the user details")
     }
 })
 
