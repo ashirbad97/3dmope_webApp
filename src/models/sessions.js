2802651ccf349e1142a58e1be35c7776a06cb32d
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-14 20:57:19 +0530

    static function to find total session of the user instance

--- src/models/sessions.js
+++ src/models/sessions.js
@@ -1,37 +1,37 @@
 const mongoose = require('mongoose')
 
 const sessionSchema = new mongoose.Schema({
-    sessionId:{
+    sessionId: {
         type: Number,
         required: true,
         unique: true
     },
-    timestamp:{
+    timestamp: {
         type: Date,
         required: true
     },
-    subjectId:{
+    subjectId: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref:'Subjects'
+        ref: 'Subjects'
     },
-    trialFilesDir:{
+    trialFilesDir: {
         type: String,
         trim: true
     },
-    outputFilesDir:{
+    outputFilesDir: {
         type: String,
         trim: true
     }
 })
 
-sessionSchema.statics.findSessionOfSubject = async(subjectId)=>{
+sessionSchema.statics.findSessionOfSubject = async (subjectId) => {
     try {
         subjectSessions = Sessions.find(subjectId).count()
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
-const Sessions = mongoose.model('Sessions',sessionSchema)
+const Sessions = mongoose.model('Sessions', sessionSchema)
 module.exports = Sessions
