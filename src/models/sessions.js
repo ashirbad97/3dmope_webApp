6c2a1e22a547a5c45913cc5c461899680a5ee8c0
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-19 15:29:27 +0530

    #10 schema statics to check for each individual session if any images exist, functions handled by lib

--- src/models/sessions.js
+++ src/models/sessions.js
@@ -1,4 +1,6 @@
+const async = require('hbs/lib/async')
 const mongoose = require('mongoose')
+const lib = require("../lib")
 
 const sessionSchema = new mongoose.Schema({
     sessionId: {
@@ -28,6 +30,24 @@ const sessionSchema = new mongoose.Schema({
 sessionSchema.statics.findSessionOfSubject = async (subjectId) => {
     try {
         subjectSessions = Sessions.find(subjectId).count()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+sessionSchema.statics.findOutputImages = async (sessionId) => {
+    try {
+        // Check if the session exist in the db
+        sessionCheck = await Sessions.findOne({ sessionId })
+        if (!sessionCheck) {
+            console.log('Session not found in MongoDB')
+            return false
+        }
+        else {
+            ifFilesExistinSessionFolder = await lib.checkIfSessionImgFolderExist(sessionId)
+            if (ifFilesExistinSessionFolder) return true
+            else return false
+        }
     } catch (error) {
         console.log(error)
     }
