d8c86d690dc8919ef7623ef46b32da2e3da5e8f9
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-20 15:09:43 +0530

    #13 trial script able to spawn a child process to call moper core

---
+++ src/utility/testSHScriptCall.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const { spawn } = require('child_process')
+/**
+   * Run python myscript, pass in `-u` to not buffer console output
+   * @return {ChildProcess}
+*/
+function runScript(sessionId) {
+    moperCorePath = path.join(__dirname, "run_moperCore.sh")
+    mrcPath = "/usr/local/MATLAB/MATLAB_Runtime/v99"
+    return spawn('bash', ["-u", moperCorePath, mrcPath, sessionId]);
+}
+const subprocess = runScript(51)
+// print output of script
+subprocess.stdout.on('data', (data) => {
+    console.log(`data:${data}`);
+});
+subprocess.stderr.on('data', (data) => {
+    console.log(`error:${data}`);
+});
+subprocess.stderr.on('close', () => {
+    console.log("Closed");
+});
